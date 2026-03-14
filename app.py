62ba2daa4c79a684c5f9a6dd315c81b7b3539bb6
Praveen <praveenprave1509@gmail.com>
2026-03-14 08:00:56 +0530

    update app.py

--- app.py
+++ app.py
@@ -518,4 +518,5 @@ def admin_delete_teacher(teacher_id):
 # ---------- RUN SERVER ----------
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
